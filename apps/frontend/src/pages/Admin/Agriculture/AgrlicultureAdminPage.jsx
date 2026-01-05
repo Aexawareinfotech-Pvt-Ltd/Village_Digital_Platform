90b11e21906b05ab3bf51bfc0adba2df94d82c83
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2026-01-05 19:49:47 +0530

    Add Agriculture page and integrate live Mandi Prices API

---
+++ apps/frontend/src/pages/Admin/Agriculture/AgrlicultureAdminPage.jsx
@@ -0,0 +1,9 @@
+import Agricultures from "../../../components/Admin/Agriculture/Agricultures";
+const Agriculture = () => {
+return (
+     <div className="min-h-screen px-4 py-6 bg-gray-50">
+       <Agricultures language="en" /> 
+     </div>
+   );
+}   
+export default Agriculture;
